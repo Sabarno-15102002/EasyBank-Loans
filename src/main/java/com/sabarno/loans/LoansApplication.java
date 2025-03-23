e098ba7018947ada7603a8ec2d51b6965c30c648
Sabarno-15102002 <sabarnobiswas2002@gmail.com>
2025-03-24 01:47:35 +0530

    EasyBanks Loans Microservices Created

---
+++ src/main/java/com/sabarno/loans/LoansApplication.java
@@ -0,0 +1,30 @@
+package com.sabarno.loans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Loans microservice REST API Documentation",
+				description = "EazyBank Loans microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Sabarno Biswas",
+						email = "sabarnobiswas2002@gmail.com"
+				)
+		)
+)
+public class LoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansApplication.class, args);
+	}
+
+}
